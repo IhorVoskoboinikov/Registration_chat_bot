a78deec8f6aaa1d0130180d85c64bbb0834961ca
YOUR_NAME <YOUR_EMAIL>
2023-04-06 16:49:52 +0300

    version for heroku fix token

--- telegram_bot_registration/registration/config.py
+++ telegram_bot_registration/registration/config.py
@@ -3,9 +3,9 @@ from collections import defaultdict
 
 USERS_PROFILE = defaultdict(dict)
 
-cwd = os.getcwd()
-with open('bot_token.txt', 'r') as f:
-    TELEGRAM_TOKEN = f.read()
+# with open('bot_token.txt', 'r') as f:
+#     TELEGRAM_TOKEN = f.read()
+TELEGRAM_TOKEN = '6095222446:AAERcOlPxiGc3hxs_sGgVFd9B7XsJ34r7M4'
 
 
 def assign_users_profile_data(message: object) -> None:
