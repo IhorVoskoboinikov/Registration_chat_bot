29fd461a68c5dbe3c645ab9e04a51f14068c1772
YOUR_NAME <YOUR_EMAIL>
2023-04-06 12:56:17 +0300

    getting a token from a file and rename assign_users_profile_data

--- telegram_bot_registration/registration/config.py
+++ telegram_bot_registration/registration/config.py
@@ -1,16 +1,14 @@
 import os
 from collections import defaultdict
-from pathlib import Path
-TELEGRAM_TOKEN = '6095222446:AAERcOlPxiGc3hxs_sGgVFd9B7XsJ34r7M4'
+
 USERS_PROFILE = defaultdict(dict)
 
 cwd = os.getcwd()
 with open('bot_token.txt', 'r') as f:
-    data = f.read()
-    print(data)
+    TELEGRAM_TOKEN = f.read()
 
 
-def get_users_profile_data(message: object) -> None:
+def assign_users_profile_data(message: object) -> None:
     USERS_PROFILE[message.chat.id]['login'] = None
     USERS_PROFILE[message.chat.id]['password'] = None
     USERS_PROFILE[message.chat.id]['first_name'] = None
