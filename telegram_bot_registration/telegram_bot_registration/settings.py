5d27af3f764ab45f8eaa04e0007def7373772e28
YOUR_NAME <YOUR_EMAIL>
2023-04-06 15:41:24 +0300

    version for heroku fix

--- telegram_bot_registration/telegram_bot_registration/settings.py
+++ telegram_bot_registration/telegram_bot_registration/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,7 +106,7 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
